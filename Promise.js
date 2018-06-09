44f0579f5d91e6b61099af9a95aee4293d51f30b
Your Name <you@example.com>
2018-06-09 13:04:30 +0800

    promise

--- Promise.js
+++ Promise.js
@@ -6,10 +6,10 @@ class Promise {
     this.onResolvedCallbacks = [];
     this.onRejectedCallbacks = [];
     let resolve = (value) => {
-      if(this.status === 'pending'){
+      if (this.status === 'pending') {
         this.status = 'resolved';
         this.value = value;
-        this.onResolvedCallbacks.forEach(fn=>fn());
+        this.onResolvedCallbacks.forEach(fn => fn());
       }
     }
     let reject = (reason) => {
@@ -19,23 +19,70 @@ class Promise {
         this.onRejectedCallbacks.forEach(fn => fn());
       }
     }
-    executor(resolve,reject);
+    try {
+      executor(resolve, reject);
+    } catch (e) {
+      reject(e); // 如果出现异常就走错误处理
+    }
   }
-  then(onFufilled,onRejected){
-    if(this.status === 'resolved'){
-      onFufilled(this.value);
+  then(onFufilled, onRejected) {
+    let promise2;
+    promise2 = new Promise((resolve, reject) => {
+      if (this.status === 'resolved') {
+        let x = onFufilled(this.value);
+        resolvePromise(promise2, x, resolve, reject);
+      }
+      if (this.status === 'rejected') {
+        let x = onRejected(this.reason);
+        resolvePromise(promise2, x, resolve, reject);
+      }
+      if (this.status === 'pending') {
+        this.onResolvedCallbacks.push(() => {
+          let x = onFufilled(this.value);
+          resolvePromise(promise2, x, resolve, reject);
+        });
+        this.onRejectedCallbacks.push(() => {
+          let x = onRejected(this.reason);
+          resolvePromise(promise2, x, resolve, reject);
+        })
+      }
+    });
+    return promise2;
+  }
+}
+// 实现多套promise共用的情况
+function resolvePromise(promise2, x, resolve, reject) {
+  if (promise2 === x) {
+    return reject(new TypeError('循环引用'));
+  }
+  // {then:{}}
+  let called;
+  if (x != null && (typeof x === 'object' || typeof x === 'function')) {
+    try {
+      let then = x.then; // 如果是对象 我就试着取一下then方法
+      if (typeof then === 'function') { // 就是promise了
+        then.call(x, y => {
+          // 成功和失败只能调用一个
+          if (called) return;
+          called = true;
+          // resolve的结果依旧是promise 那就继续解析
+          resolvePromise(promise2,y,resolve,reject);
+        }, r => {
+          if (called) return;
+          called = true;
+          reject(r);// 失败了就失败了
+        })
+      } else {
+        resolve(x); // 直接成功即可
+      }
+    } catch (e) {
+      if (called) return;
+      called = true;
+      reject(e); // 取then出错了那就不要在继续执行了
     }
-    if (this.status === 'rejected') {
-      onRejected(this.reason);
-    }
-    if( this.status === 'pending'){
-      this.onResolvedCallbacks.push(()=>{
-        onFufilled(this.value);
-      });
-      this.onRejectedCallbacks.push(()=>{
-        onRejected(this.reason);
-      })
-    }
+  } else {
+    resolve(x);
   }
 }
 module.exports = Promise;
+
---
+++ then.js
@@ -0,0 +1,11 @@
+function Promise() {
+  
+}
+
+Object.defineProperty(Promise.prototype,'then',{
+  get(){
+    throw new Error();
+  }
+})
+let p = new Promise();
+console.log(p.then)
